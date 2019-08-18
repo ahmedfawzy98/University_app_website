06ab476cd83cdfc81ad470193750884bd8c6ef82
ahmed <ahmedfawzy671998@gmailcom>
2019-08-18 14:13:37 +0200

    add Course and Student models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course1 = Course.create(short_name: 'CS111', name: 'Intro to Computer Science',
+                        description: 'An in-depth look at Computer Science as a field and prongramming in general')
+course2 = Course.create(short_name: 'CS112', name: 'Data Structures',
+                        description: 'An in-depth look at data structures')
+course3 = Course.create(short_name: 'CS205', name: 'Intro to Linear Algebra',
+                        description: 'An essential introduction to Linear Algebra
+                                      and how it impacts the modern world of machine learning')
